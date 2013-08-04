2ba5a5caa97ae0653ae824650a2730200f6c3f69
Pierre Romera <hello@pirhoo.com>
2013-08-04 16:25:48 +0200

    Added comments

--- static/nyancat.js
+++ static/nyancat.js
@@ -1,47 +1,29 @@
 (function(){
-
-    // Playfair Theme
-    // --------------
-
-    // note: this theme was originally named 'autumn'. That id
-    // is still used because we didn't want to break old charts.
-
+    // This function allow us to resiter our theme into the client-side script.
+    // We use the same id as the one defined into plugin.php and say explicitely 
+    // wich theme is its parent ('default'). This argument is optional.
     dw.theme.register('nyancat', 'default', {
-
         colors: {
+            // The palette options defined the colors available within each chart.
             palette: ["#FF9900", "#FFFF00","#33FF00", "#FE99FE", "#FF0000", "#0099FF", "#6633FF"],
             secondary: [],
             context: '#aaa',
             axis: '#fff',
-            positive: '#389486',
-            negative: '#F24405',
+            // Default color use for positive values
+            positive: '#FF9900',
+            // Default color use for neagtives values
+            negative: '#FE99FE',
             background: '#00008B'
         },
-
-        horizontalGrid: {
-            stroke: '#fff'
-        },
-
-        verticalGrid: {
-            stroke: '#fff'
-        },
-
+        // Grid's options
+        horizontalGrid: { stroke: '#fff' },
+        verticalGrid:   { stroke: '#fff' },
+        // Here a way to add option for a specific visualisation 
         columnChart: {
             cutGridLines: true
         },
-
-        frame: {
-            stroke: '#fff',
-            'stroke-width': 3,
-            fill: '#00008B',
-            'fill-opacity': 1,
-            'stroke-opacity': 1
-        },
-
         frameStrokeOnTop: true,
-
         vpadding: 10
-
     });
 
 }).call(this);
