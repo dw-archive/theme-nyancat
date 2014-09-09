d4dc413ccee74a18d0893b0c393dbba60a54694c
Gregor Aisch <gregor.aisch@nytimes.com>
2014-09-08 21:26:20 -0400

    updated to simple plugin api

---
+++ init.php
@@ -0,0 +1,23 @@
+<?php
+
+use Datawrapper\Theme;
+
+Theme::register(array(
+    // This unique id is very important
+    // to help us to identify our plugin 
+    // amoung the others 
+    'id' => 'nyancat',
+    // This option is central for this because
+    // it says that the inherits properties from the 'default' theme 
+    'extends' => 'default',            
+    // Some display options
+    'title' => 'Nyan Cat',
+    'link' => 'http://www.datawrapper.de',
+    'restricted' => NULL,
+    'version' => '0.0.0',
+    'option-filter' => array(
+        'line-chart' => array(
+            'show-grid' => true,
+        ),
+    )
+));
